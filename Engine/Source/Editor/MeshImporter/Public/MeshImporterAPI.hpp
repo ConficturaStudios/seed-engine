d1f65ca568ea7190ffe2efc5b7b9da5cda347162
Zachary Harris <developer@conficturastudios.com>
2021-04-03 00:54:34 -0400

    Added MeshImporter module to Editor. Anabled ASSIMP compilation in extern module.

---
+++ Engine/Source/Editor/MeshImporter/Public/MeshImporterAPI.hpp
@@ -0,0 +1,94 @@
+/**
+ * @file MeshImporterAPI.hpp
+ * 
+ * @copyright Copyright (c) 2021 Confictura Studios. All rights reserved.
+ * @license This code is released under the MIT License.
+ * 
+ * This code is distributed as part of the Seed Engine project.
+ * 
+ * For a copy of the license, please go to https://github.com/conficturastudios/seed-engine/LICENSE
+ */
+ 
+#ifndef SEEDENGINE_INCLUDE_EDITOR_MESH_IMPORTER_API_H_
+#define SEEDENGINE_INCLUDE_EDITOR_MESH_IMPORTER_API_H_
+
+
+
+#ifndef EDITOR_MESH_IMPORTER_EXPORT_API
+    #if BUILD_SHARED_LIBS
+        #if defined(_WIN32) || defined(__CYGWIN__) || defined(_MSC_VER)
+            #define EDITOR_MESH_IMPORTER_EXPORT_API __declspec(dllexport)
+            #define EDITOR_MESH_IMPORTER_IMPORT_API __declspec(dllimport)
+            #define EDITOR_MESH_IMPORTER_HIDDEN_API
+        #elif defined(__GNUC__) && __GNUC__ >= 4
+            #define EDITOR_MESH_IMPORTER_EXPORT_API __attribute__((visibility("default")))
+            #define EDITOR_MESH_IMPORTER_IMPORT_API __attribute__((visibility("default")))
+            #define EDITOR_MESH_IMPORTER_HIDDEN_API __attribute__((visibility("hidden")))
+        #else
+            #define EDITOR_MESH_IMPORTER_EXPORT_API
+            #define EDITOR_MESH_IMPORTER_IMPORT_API
+            #define EDITOR_MESH_IMPORTER_HIDDEN_API
+        #endif
+    #else
+        #define EDITOR_MESH_IMPORTER_EXPORT_API
+        #define EDITOR_MESH_IMPORTER_IMPORT_API
+        #define EDITOR_MESH_IMPORTER_HIDDEN_API
+    #endif
+#endif
+
+//#ifndef ${PROJECT_IDENTIFIER}_API
+#ifndef ENGINE_API
+    #ifdef EDITOR_MESH_IMPORTER_EXPORT
+        #define ENGINE_API EDITOR_MESH_IMPORTER_EXPORT_API
+    #else
+        #define ENGINE_API EDITOR_MESH_IMPORTER_IMPORT_API
+    #endif
+#endif
+
+#endif
+
+
+
+// Original:
+
+/**
+ * MeshImporterAPI.hpp
+ * 
+ * @copyright Copyright (c) 2021 Confictura Studios. All rights reserved.
+ * @license This code is released under the MIT License.
+ * 
+ * This code is distributed as part of the Seed Engine project.
+ * 
+ * For a copy of the license, please go to https://github.com/conficturastudios/seed-engine/LICENSE
+ */
+ 
+/*
+#ifndef SEEDENGINE_INCLUDE_EDITOR_MESH_IMPORTER_API_H_
+#define SEEDENGINE_INCLUDE_EDITOR_MESH_IMPORTER_API_H_
+
+
+
+#ifndef ENGINE_API
+
+    #if defined(_WIN32) && defined(BUILD_SHARED_LIBS)
+        #if BUILD_SHARED_LIBS
+            #if defined(_MSC_VER)
+                #pragma warning(disable: 4251)
+            #endif
+
+            #if defined(EDITOR_MESH_IMPORTER_EXPORT)
+                #define ENGINE_API __declspec(dllexport)
+            #else
+                #define ENGINE_API __declspec(dllimport)
+            #endif
+        #else
+            #define ENGINE_API
+        #endif
+    #else
+        #define ENGINE_API
+    #endif
+    
+#endif
+
+#endif
+*/
