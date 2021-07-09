62f9b71c84dbf920f6d4d7fd4930350bfda21572
Zachary Harris <developer@conficturastudios.com>
2021-07-09 04:11:33 -0400

    Finished Stack collection implementation using a linked list adapter.

--- Engine/Source/Runtime/Collections/Public/Stack.hpp
+++ Engine/Source/Runtime/Collections/Public/Stack.hpp
@@ -14,14 +14,21 @@
 
 #include "CollectionsAPI.hpp"
 
+#include "Collection.hpp"
+
 namespace seedengine {
 
     /**
-     * @brief
+     * @brief A last-in first-out stack of generic data.
      * @details
-     * 
+     * A first-in last-out stack of generic data built on top of a linked list implementation.
+     * It is worth noting that this stack implementation is not what would be considered a pure stack in that
+     * it allows for all generic collection operations and iteration without removal. This may be changed
+     * in a later update.
+     * @tparam T The type of data stored inside of this stack.
      */
-    class Stack {
+    template <typename T>
+    class Stack : public Collection<T> {
 
         public:
 
@@ -31,8 +38,8 @@ namespace seedengine {
              * @brief The default constructor for Stack objects.
              * @details Constructs a new Stack with default initialization for all members.
              */
-            Stack() {
-                
+            Stack() : Collection<T>() {
+                m_list();
             }
 
             /**
@@ -51,13 +58,83 @@ namespace seedengine {
              * @brief The destructor for Stack objects.
              * @details Called when an instance of Stack is deleted.
              */
-            virtual ~Stack() {
-                
-            }
+            virtual ~Stack() = default;
 
         // Functions
 
+            /**
+             * Pushes the provided value onto the top of this stack.
+             * @param value The value to add to this stack.
+             */
+            void push(const T& value) {
+                m_list.addLast(value);
+            }
 
+            /**
+             * Removes and returns the element at the top of this stack.
+             * @return The element previously on the top of this stack.
+             */
+            T pop() {
+                return m_list.removeLast();
+            }
+
+            /**
+             * Checks the value on the top of this stack without removing it.
+             * @return A reference to the value on the top of this stack.
+             */
+            [[nodiscard]] T& peek() {
+                return m_list.last();
+            }
+
+            /**
+             * Checks the value on the top of this stack without removing it.
+             * @return A reference to the value on the top of this stack.
+             */
+            [[nodiscard]] const T& peek() const {
+                return m_list.last();
+            }
+
+            [[nodiscard]] size_t size() const noexcept override {
+                return m_list.size();
+            }
+
+            bool removeElement(const T& element) override {
+                return m_list.removeElement(element);
+            }
+
+            [[nodiscard]] bool contains(const T& element) const override {
+                return m_list.contains(element);
+            }
+
+            void clear() override {
+                m_list.clear();
+            }
+
+        // Iterators
+
+            [[nodiscard]] Iterator<T> begin() override {
+                return m_list.begin();
+            }
+
+            [[nodiscard]] const Iterator<T> begin() const override {
+                return m_list.begin();
+            }
+
+            [[nodiscard]] const Iterator<T> cbegin() const override {
+                return m_list.cbegin();
+            }
+
+            [[nodiscard]] Iterator<T> end() override {
+                return m_list.end();
+            }
+
+            [[nodiscard]] const Iterator<T> end() const override {
+                return m_list.end();
+            }
+
+            [[nodiscard]] const Iterator<T> cend() const override {
+                return m_list.cend();
+            }
 
         // Assignment Operators
 
@@ -73,9 +150,10 @@ namespace seedengine {
              */
             Stack& operator=(Stack&& rhs) noexcept = default;
 
-        protected:
-
         private:
+
+            /** The internal linked list representing this stack. */
+            LinkedList<T> m_list;
 
     };
 
